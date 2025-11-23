02db68f6b58aecc22deebcfcd7cbe4fa71e9d02b
DESKTOP-MAIBTV1\A <anfemail@gmail.com>
2025-11-23 19:36:49 +0300

    redis simple tests

---
+++ tests/Vertr.TinvestGateway.Tests/RedisTests/SimpleRedisConnectionTests.cs
@@ -0,0 +1,41 @@
+﻿using StackExchange.Redis;
+
+namespace Vertr.TinvestGateway.Tests.RedisTests;
+
+// https://stackexchange.github.io/StackExchange.Redis/Basics
+public class SimpleRedisConnectionTests
+{
+    [Test]
+    public async Task CanConnectToRedis()
+    {
+        var redis = ConnectionMultiplexer.Connect("localhost");
+        var db = redis.GetDatabase();
+
+        var value = "abcdefg";
+        await db.StringSetAsync("mykey", value);
+
+        var saved = await db.StringGetAsync("mykey");
+        Console.WriteLine(saved);
+
+        Assert.Pass();
+    }
+
+    [Test]
+    public async Task CanUsePubSub()
+    {
+        var redis = ConnectionMultiplexer.Connect("localhost");
+        var sub = redis.GetSubscriber();
+
+        sub.Subscribe("messages").OnMessage(async channelMessage => 
+        {
+            await Task.Delay(1000);
+            Console.WriteLine(channelMessage.Message);
+        });
+
+        sub.Publish("messages", "hello");
+
+        await Task.Delay(5000);
+
+        Assert.Pass();
+    }
+}
